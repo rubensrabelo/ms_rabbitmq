ef90c3c6fbd7f6d11b3ad2d38e82e03889c4764d
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-09-22 13:40:29 -0300

    chore(project): add product microservice

---
+++ backend/product/src/test/java/io/github/rubensrabelo/ms/product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.rubensrabelo.ms.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
