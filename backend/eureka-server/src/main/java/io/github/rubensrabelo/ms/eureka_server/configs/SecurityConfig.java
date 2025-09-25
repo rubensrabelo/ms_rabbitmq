c196d117b65aaa89a95861a52488fc6df214041a
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-09-25 09:50:13 -0300

    feat(backend-eureka): add microservice

---
+++ backend/eureka-server/src/main/java/io/github/rubensrabelo/ms/eureka_server/configs/SecurityConfig.java
@@ -0,0 +1,21 @@
+package io.github.rubensrabelo.ms.eureka_server.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+}
